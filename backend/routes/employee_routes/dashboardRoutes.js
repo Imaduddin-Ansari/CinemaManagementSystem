2dfb5745e34501c8d78d59c6a286d13774c09cef
Bushra Sajid <i222613@nu.edu.pk>
2024-12-07 15:24:37 +0500

    deleted

--- backend/routes/employee_routes/dashboardRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getDashboardData } = require('../../controllers/employee_controllers/dashboardController');
-const authenticate = require('../../middleware/auth');
-const { isEmployee } = require('../../middleware/auth');
-// Route for fetching dashboard data
-router.get('/dashboard',authenticate.authenticate,isEmployee, getDashboardData);
-
-module.exports = router;
