7292f0ed832012558e956f5af6f61486af082513
Imaduddin Ansari <imad.ansarilol@gmail.com>
2024-12-09 08:12:21 +0500

    Removing Old Version as not compatible to merge and pull

--- backend/models/Report.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const reportSchema = new mongoose.Schema({
-    generatedBy: { type:mongoose.Schema.Types.ObjectId , ref: 'User', required: true }, // Employee generating the report
-    date: { type: Date, required: true },
-    type: { type: String, enum: ['booking', 'inquiry', 'other'], required: true },
-    data: { type: mongoose.Schema.Types.Mixed, required: true }, // Flexible field for storing report data
-  }, { timestamps: true });
-  
-  module.exports = mongoose.model('Report', reportSchema);
